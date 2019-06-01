09d1a41d20d5c14b94a82b4af7da77b96d424507
Justin Elliott <Justinelliott11375@gmail.com>
2019-06-01 15:30:27 -0500

    looping over ingredients, but building entire drinks

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function drink(prepVessel, glassware, method, ice, garnish, muddleInsert, bitter
 
 var manhattan = new drink("mixing glass", "coupe", "stir", null, "cherry and expressed orange peel", null, ["1 Dash Angostura Bitters", "1 Dash Orange Bitters"], null, null, ["1 oz Sweet Vermouth"], ["2 oz Rye Whiskey"]);
 var martini = new drink("mixing glass", "coupe", "stir", null, "lemon peel", null, ["1 Dash Orange Bitters"], null, null, ["1 oz Dry Vermouth"], ["2 oz Gin"]);
-var mojito = new drink("large shaker tin", "collins glass", "shake", "collins spear", "mint sprig", ["7-10 muddled mint leaves"], null, [".75 oz simple syrup"], [".75 oz lime juice"], null, ["2 oz light rum"]);
+var mojito = new drink("large shaker tin", "collins glass", "shake", "collins spear", ["mint sprig"], ["7-10 muddled mint leaves"], null, [".75 oz simple syrup"], [".75 oz lime juice"], null, ["2 oz light rum"]);
 var vodkaSoda = new drink(null, "rocks glass", "build", "kold draft", "lime wedge");
 var daiquiri = new drink("large shaker tin", "coupe", "shake", null, "lime wheel");
 
@@ -37,10 +37,10 @@ function makeDrinkOrder(array) {
     for (let i = 0; i < array.length; i++) {
 
         if (array[i].method == "stir") {
-            console.log("fired stir loop");
             for (let j = 0; j < array[i].recipe.length; j++) {
-                console.log(j);
-                console.log("In " + array[i].prepVessel + " " + (i + 1) + " add " + array[i].recipe[0]);
+                if (array[i].recipe[j] !== null) {
+                    console.log("In " + array[i].prepVessel + " " + (i + 1) + " add " + array[i].recipe[j]);
+                }
             }
         }
     }
