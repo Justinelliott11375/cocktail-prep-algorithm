49859310e25117b16f6fc5dcba156d5bb4d5d7a1
Justin Elliott <Justinelliott11375@gmail.com>
2019-06-05 17:50:32 -0500

    add initial loop for shaken drinks, started next loop for building across

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function drink(prepVessel, glassware, method, ice, garnish, muddleInsert, bittersDropper, syrup, juice, modifier, baseSpirit) {
+function drink(prepVessel, glassware, method, ice, garnish, muddleInsert, bittersDropper, syrup, juice, modifier, baseSpirit, lengthener) {
     this.prepVessel = prepVessel;
     this.glassware = glassware;
     this.method = method;
@@ -10,19 +10,21 @@ function drink(prepVessel, glassware, method, ice, garnish, muddleInsert, bitter
     this.juice = juice;
     this.modifier = modifier;
     this.baseSpirit = baseSpirit;
-    this.recipe = [this.muddleInsert, this.bittersDropper, this.syrup, this.juice, this.modifier, this.baseSpirit];
+    this.lengthener = lengthener;
+    this.recipe = [this.muddleInsert, this.bittersDropper, this.syrup, this.juice, this.modifier, this.baseSpirit, this.lengthener];
 
 }
 
-var manhattan = new drink("mixing glass", "coupe", "stir", null, "cherry and expressed orange peel", null, ["1 Dash Angostura Bitters", "1 Dash Orange Bitters"], null, null, ["1 oz Sweet Vermouth"], ["2 oz Rye Whiskey"]);
-var martini = new drink("mixing glass", "coupe", "stir", null, "lemon peel", null, ["1 Dash Orange Bitters"], null, null, ["1 oz Dry Vermouth"], ["2 oz Gin"]);
-var mojito = new drink("large shaker tin", "collins glass", "shake", "collins spear", ["mint sprig"], ["7-10 muddled mint leaves"], null, [".75 oz simple syrup"], [".75 oz lime juice"], null, ["2 oz light rum"]);
-var vodkaSoda = new drink(null, "rocks glass", "build", "kold draft", "lime wedge");
-var daiquiri = new drink("large shaker tin", "coupe", "shake", null, "lime wheel");
+var manhattan = new drink("mixing glass", "coupe", "stir", null, "cherry and expressed orange peel", null, ["1 Dash Angostura Bitters", "1 Dash Orange Bitters"], null, null, ["1 oz Sweet Vermouth"], ["2 oz Rye Whiskey"], null);
+var martini = new drink("mixing glass", "coupe", "stir", null, "lemon peel", null, ["1 Dash Orange Bitters"], null, null, ["1 oz Dry Vermouth"], ["2 oz Gin"], null);
+var mojito = new drink("large shaker tin", "collins glass", "shake", "collins spear", ["mint sprig"], ["7-10 muddled mint leaves"], null, [".75 oz simple syrup"], [".75 oz lime juice"], null, ["2 oz light rum"], ["Soda water"]);
+var vodkaSoda = new drink(null, "rocks glass", "build", "kold draft", "lime wedge", null, null, null, null, null, ["1.5 oz Vodka"], ["Soda water"]);
+var daiquiri = new drink("large shaker tin", "coupe", "shake", null, "lime wheel", null, null, [".75 oz simple syrup"], [".75 oz lime juice"], null, ["2 oz light rum"], null);
 
-var orderArray = [manhattan, martini, mojito, vodkaSoda, daiquiri, manhattan];
+var orderArray = [manhattan, manhattan, martini, mojito, vodkaSoda, daiquiri];
 
 function makeDrinkOrder(array) {
+    //start timer here
     var groupCounter = 0;
     console.log("Mise en place: ")
     for (let i = 0; i < array.length; i++) {
@@ -34,15 +36,50 @@ function makeDrinkOrder(array) {
             console.log(array[i].glassware);
         }
     }
+    var drinkLoopCounter = 0;
+    var ingredientGroupCounter = 0;
+    var ingredientLoopCounter = 0;
     console.log("Ingredients: ");
     for (let drinkIndex = 0; drinkIndex < array.length; drinkIndex++) {
-
+        //drinkLoopCounter += 1;
+        //console.log("drinkLoopCounter: " + drinkLoopCounter);
         if (array[drinkIndex].method == "stir") {
             for (let groupIndex = 0; groupIndex < array[drinkIndex].recipe.length; groupIndex++) {
+                //ingredientGroupCounter += 1;
+                //console.log("ingredientGroupCounter: " + ingredientGroupCounter); 
                 if (array[drinkIndex].recipe[groupIndex] !== null) {
                     for (let ingredientIndex = 0; ingredientIndex < array[drinkIndex].recipe[groupIndex].length; ingredientIndex++) {
                         //console.log("k loop");
+                        //ingredientLoopCounter += 1;
+                        //console.log("ingredientLoopCounter: " + ingredientLoopCounter);
                         console.log("In " + array[drinkIndex].prepVessel + " " + (drinkIndex + 1) + " add " + array[drinkIndex].recipe[groupIndex][ingredientIndex]);
+                        /*for(let j = drinkIndex+1; j < array.length; j++){
+                          console.log("next drink: " + array[drinkIndex+1].recipe[groupIndex][ingredientIndex]);
+                          console.log("current drink: " + array[drinkIndex].recipe[groupIndex][ingredientIndex]);
+                          if(array[j].recipe[groupIndex][ingredientIndex].includes(array[drinkIndex].recipe[groupIndex][ingredientIndex])){
+                            console.log("yes");
+                          };
+                        }*/
+                    }
+                }
+            }
+        }
+        if (array[drinkIndex].method !== "stir") {
+            for (let groupIndex = 0; groupIndex < array[drinkIndex].recipe.length; groupIndex++) {
+                //ingredientGroupCounter += 1;
+                //console.log("ingredientGroupCounter: " + ingredientGroupCounter); 
+                if (array[drinkIndex].recipe[groupIndex] !== null) {
+                    for (let ingredientIndex = 0; ingredientIndex < array[drinkIndex].recipe[groupIndex].length; ingredientIndex++) {
+                        //ingredientLoopCounter += 1;
+                        //console.log("ingredientLoopCounter: " + ingredientLoopCounter);
+                        console.log("In " + array[drinkIndex].prepVessel + " " + (drinkIndex + 1) + " add " + array[drinkIndex].recipe[groupIndex][ingredientIndex]);
+                        /*for(let j = drinkIndex+1; j < array.length; j++){
+                          console.log("next drink: " + array[drinkIndex+1].recipe[groupIndex][ingredientIndex]);
+                          console.log("current drink: " + array[drinkIndex].recipe[groupIndex][ingredientIndex]);
+                          if(array[j].recipe[groupIndex][ingredientIndex].includes(array[drinkIndex].recipe[groupIndex][ingredientIndex])){
+                            console.log("yes");
+                          };
+                        }*/
                     }
                 }
             }
@@ -50,3 +87,5 @@ function makeDrinkOrder(array) {
     }
 
 }
+
+//remove lengtheners from build process!!!
